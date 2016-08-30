147d8d2906086981b3261c685fe76ac0ee4a2cf9
joshhsoj1902 <joshhsoj1902@gmail.com>
2016-08-29 22:20:16 -0400

    Added mongoDB support & updated routes.

--- app.js
+++ app.js
@@ -1,37 +1,88 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-app.use(router);
+//app.use(router);
+//app.use('/api', bodyParser.json());
 
+ app.use('/api',bodyParser.text()); 									// Allows bodyParser to look at raw text
+ app.use('/api',bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+ app.use('/api',bodyParser.json());                                     // parse application/json
+ app.use('/api',bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use('/api', router);
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/palindrome');
 
+var Message = require('./models/message');
 
-router.get('/message/:messageId', function (req, res) {
+// Get single message
+router.get('/api/message/:messageId', function (req, res) {
     console.log("Get single message");
-    // Get single message
-});  
-router.get('/message', function (req, res) {
+    
+    Message.findById(req.params.messageId, function(err, message) {
+        if (err) throw err;
+
+        console.log(message);
+        res.send(message);
+    });
+});
+ // Get all message
+router.get('/api/message', function (req, res) {
     console.log("Get all messages");
-    // Get single message
+    Message.find({}, function (err, messages) {
+        if (err) throw err;
+        console.log(messages);
+        res.send(messages);
+    });
 });
-router.put('/message/:messageId', function (req, res) { 
+router.put('/api/message/:messageId', function (req, res) {
     console.log("put single message");
-    // update message 
+
+    Message.findById(req.params.messageId, function(err, message) {
+        if (err) throw err;
+
+        message.body = req.body.body;
+
+         message.save(function(err) {
+            if (err) throw err;
+
+            console.log('Message successfully updated!');
+        });
+
+    });
+    
 });
-router.put('/message', function (req, res) { 
+router.put('/message', function (req, res) {
     console.log("add new message");
+
+    var message = new Message({
+        body: req.body.body
+    });
+
+    message.save(function (err) {
+        if (err) throw err;
+
+        console.log('Message saved successfully!');
+    });
+
     // update message 
 });
 router.delete('/message/:messageId', function (req, res) {
-    console.log("delete single message"); 
-    // do something 
-});  
+    console.log("delete single message");
+    Message.findByIdAndRemove(req.params.messageId, function(err) {
+        if (err) throw err;
+
+        console.log('Message deleted!');
+    });
+});
 
 router.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('App is listening on port 3000!');
+    console.log('App is listening on port 3000!');
 });
---
+++ models/message.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+    // Mongo has a built in ID, consider hashing it before exposing it:
+    //http://stackoverflow.com/questions/16329353/use-the-database-id-as-the-restful-service-id-expose-a-threat
+    // id: { type: String, required: true, unique: true },
+  body: { type: String, required: true},
+  isPalindrome: Boolean,
+  created_at: Date,
+  updated_at: Date
+});
+
+// on every save, add the Date and check if it's a Palindrome
+messageSchema.pre('save', function(next) {
+  var currentDate = new Date();  
+  this.updated_at = currentDate;
+
+  //Only set the created date if it isn't already set (adding)
+  if (!this.created_at){
+    this.created_at = currentDate;
+  }
+
+  this.isPalindrome = false;
+
+  next();
+});
+
+var Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
