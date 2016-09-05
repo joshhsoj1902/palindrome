37a6de99b8b1836cd76f9570087ae057c9b7922e
joshhsoj1902 <joshhsoj1902@gmail.com>
2016-09-05 17:45:41 -0400

    Remove unneeded TODO

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 var config = {};
 
-//TODO: Change this to always use the environment var if it is defined
 config.mongoURI = {
   development: process.env.MONGO_URL||'mongodb://localhost/palindrome-dev',
   test: process.env.MONGO_URL||'mongodb://localhost/palindrome-test',
