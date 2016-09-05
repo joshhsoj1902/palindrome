f5e7ab8e7718c7425eef4b228b782d65194ff62a
joshhsoj1902 <joshhsoj1902@gmail.com>
2016-09-05 17:42:52 -0400

    Always use MONGO_URL before the hardcoded value

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 var config = {};
 
+//TODO: Change this to always use the environment var if it is defined
 config.mongoURI = {
-  development: 'mongodb://localhost/palindrome-dev',
-  test: 'mongodb://localhost/palindrome-test',
-  production: process.env.MONGO_URL
+  development: process.env.MONGO_URL||'mongodb://localhost/palindrome-dev',
+  test: process.env.MONGO_URL||'mongodb://localhost/palindrome-test',
+  production: process.env.MONGO_URL||'mongodb://localhost/palindrome'
 };
 
 module.exports = config;
